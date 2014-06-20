a41016f6ed50fc8917dacdf2cef628741c447f37
joseen <qs.joseen@gmail.com>
2014-06-20 11:36:51 +0800

    修改提示框样式

--- lib/bootstrap_helper/helper.rb
+++ lib/bootstrap_helper/helper.rb
@@ -30,8 +30,8 @@ module BootstrapHelper
     def notice_message
       flash_messages = []
       flash.each do |type, message|
-        type = :success if type == :notice
-        text = content_tag(:div, link_to("x", "#", :class => "close", "data-dismiss" => "alert") + message, :class => "alert fade in alert-#{type}")
+        type = :success if type.to_sym == :notice
+        text = content_tag(:div, link_to("x", "#", :class => "close", "data-dismiss" => "alert") + message, :class => "alert alert-#{type}")
         flash_messages << text if message
       end
       flash_messages.join("\n").html_safe
